cea3831483f0efe83e178a92547ed5b7fbe99b5a
Jason Livesay <ithkuil@gmail.com>
2015-01-28 03:00:43 +0000

    update

--- src.js
+++ src.js
@@ -40,14 +40,71 @@ var Graph = function Graph(map) {
     }
     ;
   }}, {});
+var DijkstrasGraph = function DijkstrasGraph(map) {
+  "use strict";
+  $traceurRuntime.superConstructor($DijkstrasGraph).call(this, map);
+  this.unvisited = new Set(this.vertices);
+};
+var $DijkstrasGraph = DijkstrasGraph;
+($traceurRuntime.createClass)(DijkstrasGraph, {
+  markVisited: function(vertex) {
+    "use strict";
+    vertex.visited = true;
+    write('marked vertex ' + vertex.name + ' as visited');
+    this.unvisited.delete(vertex);
+  },
+  findNextClose: function() {
+    "use strict";
+    var next = null;
+    var closest = Infinity;
+    for (var $__1 = this.unvisited[$traceurRuntime.toProperty(Symbol.iterator)](),
+        $__2 = void 0; !($__2 = $__1.next()).done; ) {
+      var vertex = $__2.value;
+      {
+        if (vertex.distance === undefined || vertex.distance <= closest) {
+          next = vertex;
+          write(vertex.name);
+        }
+      }
+    }
+    return next;
+  },
+  findShortestPath: function(from, to) {
+    "use strict";
+    from.edges.map((function(edge) {
+      var vertex = edge.endVertex;
+      if (!vertex.visited) {
+        var distance = from.distance + edge.weight;
+        if (distance < vertex.distance) {
+          vertex.distance = distance;
+        }
+      }
+    }));
+    this.markVisited(from);
+    var next = this.findNextClose();
+    if (next === to) {
+      return true;
+    } else {
+      if (next != null)
+        return this.findShortestPath(next, to);
+    }
+  },
+  findShortest: function(a, b) {
+    "use strict";
+    return this.findShortestPath(this.map[a].obj, this.map[b].obj);
+  }
+}, {}, Graph);
+write('Hello from testgraph');
 var map = {
   a: {
     b: 1,
     c: 2
   },
-  b: {a: 1},
-  c: {a: 2}
+  b: {
+    a: 1,
+    c: 1
+  },
+  c: {b: 1}
 };
-var graph = new Graph(map);
-write('Hello from testgraph');
-write(JSON.stringify(graph));
+var graph = new DijkstrasGraph(map);
+graph.findShortest('a', 'b');
--- src/testgraph.js
+++ src/testgraph.js
@@ -38,9 +38,70 @@ class Graph {
 }
 
 
-var map = { a: { b:1, c: 2}, b: { a: 1 }, c: { a: 2 } };
+class DijkstrasGraph extends Graph {
 
-var graph = new Graph(map);
+  constructor(map) {
+    super(map);
+    this.unvisited = new Set(this.vertices);
+  }
+
+  markVisited(vertex) {
+    vertex.visited = true;
+    write('marked vertex ' + vertex.name + ' as visited');
+    this.unvisited.delete(vertex);
+  }
+
+  findNextClose() {
+    var next = null;
+    var closest = Infinity;
+    for (var vertex of this.unvisited) {
+      if (vertex.distance === undefined 
+          || vertex.distance <= closest) {
+        next = vertex;
+        write(vertex.name);
+      }
+    }
+    return next;
+  }
+
+  findShortestPath(from, to) {
+    from.edges.map((edge) => { 
+      var vertex = edge.endVertex;
+      if (!vertex.visited) { 
+        var distance = from.distance + edge.weight;
+        if (distance < vertex.distance) {
+          vertex.distance = distance;
+        }
+      }
+    });
+
+    this.markVisited(from);
+
+    var next = this.findNextClose();
+
+    if (next === to) {
+      return true;
+    } else {
+      if (next != null) 
+        return this.findShortestPath(next, to);
+    }
+  }
+
+  findShortest(a, b) {
+    return this.findShortestPath(this.map[a].obj, this.map[b].obj);  
+  }
+}
+
 
 write('Hello from testgraph');
-write(JSON.stringify(graph));
+
+var map = { a: { b:1, c: 2}, b: { a: 1, c: 1 }, c: { b: 1 } };
+
+var graph = new DijkstrasGraph(map);
+
+
+graph.findShortest('a', 'b');
+
+
+
+
